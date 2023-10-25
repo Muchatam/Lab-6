0e01a24304c6e85130cabf622aff9ddbc121f7be
Muchatam <86578095+Muchatam@users.noreply.github.com>
2023-10-25 12:43:40 -0400

    Add files via upload

---
+++ encode-decode.py
@@ -0,0 +1,39 @@
+# Micah Tam
+
+def main():
+    while True:
+        print('''Menu
+-------------
+1. Encode
+2. Decode
+3. Quit
+''')
+        choice = int(input('Please enter an option: '))
+        if choice == 1:
+            password = input('Please enter your password to encode: ')
+            encoded_password = encode(password)
+            print('Your password has been encoded and stored!')
+        elif choice == 2:
+            decoded_password = decode(encoded_password)
+            print(f'The encoded password is {encoded_password}, and the original password is {decoded_password}.')
+        elif choice == 3:
+            break
+
+
+def encode(password):
+    list_pass = [*password]
+    for i in range(len(list_pass)):
+        list_pass[i] = int(list_pass[i]) + 3
+        if list_pass[i] >= 10:
+            list_pass[i] -= 10
+    str_list = [str(i) for i in list_pass]
+    encoded_pass = ''.join(str_list)
+    return encoded_pass
+
+
+def decode(encoded_password):
+    pass
+
+
+if __name__ == '__main__':
+    main()
